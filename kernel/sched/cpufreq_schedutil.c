838fc2b99f8eff4f8e18aed149b831651d012ab9
Yahya Wessam <yahyawessam2002@gmail.com>
2025-06-14 16:17:17 +0300

    schedutil: tune rate_limits to Ktweak balance
    Reference: https://github.com/FlopKernel-Series/flop_ginkgo_kernel/commit/27a2d9528c1f9bd58d1c489faa1657539e79bb93
    
    Co-authored-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -21,8 +21,8 @@
 #include "sched.h"
 
 #define SUGOV_KTHREAD_PRIORITY	50
-#define UP_RATE_LIMIT_US		(1000)
-#define DOWN_RATE_LIMIT_US		(1000)
+#define UP_RATE_LIMIT_US		(4000)
+#define DOWN_RATE_LIMIT_US		(16000)
 
 struct sugov_tunables {
 	struct gov_attr_set attr_set;
