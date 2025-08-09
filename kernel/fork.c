b1c737b8b473d8720920121028cc14799abeeac9
Yahya Wessam <yahyawessam2002@gmail.com>
2025-08-10 02:54:55 +0300

    Revert "kernel: Boost DDR bus for a short amount of time when zygote forks"
    This reverts commit db3c2bde1cd692fa08c67ef559944c2b547cab5b.
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,9 +95,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 
-#include <linux/devfreq_boost.h>
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -2232,10 +2230,9 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
 		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
 	}
 
 	/*
